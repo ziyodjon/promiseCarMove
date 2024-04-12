75563aea8de793a4228a5dda39f2c96930099345
Ziyodulla <admin@jbnuu.uz>
2024-04-12 20:54:11 +0500

    LastChanges

--- index.js
+++ index.js
@@ -1,30 +1,35 @@
 const car1 = document.querySelector('.car1');
+const car2 = document.querySelector('.car2');
+const car3 = document.querySelector('.car3');
 const btn = document.querySelector('.btn');
 const finish = document.querySelector('.finish');
+let carMoveAudio = new Audio('mp3/carmove.mp3');
+let carStopAudio = new Audio('mp3/carstop.mp3');
 
-function carMove(){
-    let audio = new Audio('mp3/carmove.mp3');
-    audio.play();
-}
+// function carMove(){
+//     let audio = new Audio('mp3/carmove.mp3');
+//     audio.play();
+// }
 
 function carStop(){
     let audio = new Audio('mp3/carstop.mp3');
     audio.play();
 }
 
-function createCar(steps,msTime){
+function createCar(car,steps,msTime,finishSteps){
     const promise = new Promise((resolve,reject) => {
         let carSteps = steps;
         
         btn.addEventListener('click',() =>{
-            carMove();
+            carMoveAudio.play();
             let moveTimer = setInterval(() => {
                 carSteps++;
-                car1.style.left = `${carSteps}px`;
+                car.style.left = `${carSteps}px`;
                 
-                if(carSteps === 750){
-                    
+                if(carSteps === finishSteps){
                     clearInterval(moveTimer);
+                    carMoveAudio.stop();
+                    carStopAudio.play();
                     resolve();
                 }
             },msTime);
@@ -37,11 +42,25 @@ function createCar(steps,msTime){
     return promise;
 }
 
-let promise = createCar(0,10);
+let promise = createCar(car1,0,10,300);
+let promise2 = createCar(car2,0,10,500);
+let promise3 = createCar(car3,0,10,750);
 
 
 promise.then(() => {
     finish.innerHTML = 'Car finished';
+    carStop();
+    console.log('Promise DONE');
+});
+
+promise2.then(() => {
+    finish.innerHTML = 'Car 2 finished ';
+    carStop();
+    console.log('Promise DONE');
+});
+
+promise3.then(() => {
+    finish.innerHTML = 'Car 3 finished';
     carStop();
     console.log('Promise DONE');
 });
