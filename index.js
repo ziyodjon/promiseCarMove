d2f70dee7db8363f7f19c0ebf19e61dccd275201
Ziyodulla <admin@jbnuu.uz>
2024-04-14 02:10:28 +0500

    Сделал все варианты то что вы сказали

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const car2 = document.querySelector('.car2');
 const car3 = document.querySelector('.car3');
 const btn = document.querySelector('.btn');
 const finish = document.querySelector('.finish');
+
 let carMoveAudio = new Audio('mp3/carmove.mp3');
 let carStopAudio = new Audio('mp3/carstop.mp3');
 
 
 function createCar(carName,car,carSteps,msTime,finishSteps){
-    const promise = new Promise((resolve,reject) => {
+    const promise = new Promise((resolve) => {
 
         btn.addEventListener('click',() =>{
             carMoveAudio.play();
@@ -20,8 +21,10 @@ function createCar(carName,car,carSteps,msTime,finishSteps){
                 if(carSteps === finishSteps){
                     carMoveAudio.pause();
                     carStopAudio.play();
+
                     clearInterval(moveTimer);
-                    resolve([carName,carSteps]);
+                    resolve({carName,carSteps});
+                    //resolve([carName,carSteps]);
                     btn.setAttribute('disabled','');
                 }
 
@@ -40,15 +43,59 @@ let promise2 = createCar('BMW',car2,0,10,700);
 let promise3 = createCar('BYD',car3,0,10,850);
 
 Promise.all([promise,promise2,promise3]).then(datas => {
-    for(let i = 0; i <= datas.length; i++){
+
+    // Это финишний код с помощью объекта resolve({carName,carSteps});
+    datas.forEach(carInfo => {
+
+        // Тут мы добавляем элемент див и к нему добавляем класс
         let carResEl = document.createElement('div');
         carResEl.classList.add('carmoveresult'); 
-        const carName = datas[i][0];
-        const carDist = datas[i][1];
-        const AllCarsResult = `Машина ${carName} закончил свой путь и проехала ${carDist} метров`;
+
+        // Тут идет процесс диструктизация ))
+        const {carName, carSteps} = carInfo;
+
+        // Собираем все в одну кучу
+        const AllCarsResult = `Машина ${carName} закончил свой путь и проехала ${carSteps} метров`;
+
+        // Добавляем нашу кучу созданный нами элемент
         carResEl.append(AllCarsResult);
+
+        // Наш элимент с кучой добавляем в элемент финиш
         finish.append(carResEl);
-    }
+    });
+
+    // Тут я сделал с помощью массива и все получилось resolve([carName,carSteps]);
+
+    // datas.forEach(carInfo => {
+
+    //     // Тут мы добавляем элемент див и к нему добавляем класс
+    //     let carResEl = document.createElement('div');
+    //     carResEl.classList.add('carmoveresult'); 
+
+    //     // Тут идет процесс диструктизация ))
+    //     const [carName, carDist] = carInfo;
+
+    //     // Собираем все в одну кучу
+    //     const AllCarsResult = `Машина ${carName} закончил свой путь и проехала ${carDist} метров`;
+
+    //     // Добавляем нашу кучу созданный нами элемент
+    //     carResEl.append(AllCarsResult);
+
+    //     // Наш элимент с кучой добавляем в элемент финиш
+    //     finish.append(carResEl);
+    // });
+
+    // Это старый код то что я сделал
+
+    // for(let i = 0; i <= datas.length; i++){
+    //     let carResEl = document.createElement('div');
+    //     carResEl.classList.add('carmoveresult'); 
+    //     const carName = datas[i][0];
+    //     const carDist = datas[i][1];
+    //     const AllCarsResult = `Машина ${carName} закончил свой путь и проехала ${carDist} метров`;
+    //     carResEl.append(AllCarsResult);
+    //     finish.append(carResEl);
+    // }
 });
 
 
